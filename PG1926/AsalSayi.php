0765710cbf28228ebdb032aa3ab61763ab4c2d75
BYMasterX <74146554+BYMasterX@users.noreply.github.com>
2021-02-13 22:51:50 +0300

    Add files via upload

---
+++ PG1926/AsalSayi.php
@@ -0,0 +1,21 @@
+<?php
+      $kontrol=0;
+      $sayi= $_POST['sayi'];
+      if(is_numeric($sayi)){
+       for ($a=2; $a<=($sayi-1) ; $a++)
+         {
+         if($sayi%$a==0)
+         {
+           $kontrol=1;
+           echo "Sayınız asal sayı değildir, Çünkü $a ' e tam olarak bölünmektedir.";
+           break;
+         }
+         }
+          if($kontrol==0)
+         {
+          echo " Sayınız asal sayıdır, Çünkü hiçbir sayıya tam bölünmemektedir.";
+          }
+        }else{
+          echo "Lütfen bir sayı giriniz.";
+        }
+      ?>
---
+++ PG1926/GunBolumleri.php
@@ -0,0 +1,25 @@
+<?php
+date_default_timezone_set('Europe/Istanbul');
+$bugün= date("H");
+
+if ($bugün >= "06" AND $bugün < "10")
+{
+	echo 'Günaydın';
+}
+elseif ($bugün >= "10" AND $bugün < "17")
+{
+	echo 'İyi günler';
+}
+elseif ($bugün >= "17" AND $bugün < "20")
+{
+	echo 'İyi akşamlar';
+}
+elseif ($bugün >= "20" AND $bugün < "00")
+{
+	echo 'İyi geceler';
+}
+else
+{
+	echo 'Esenlikler dilerim';
+}
+?>
